6f83369f3bd4e374f2046547e505d3cdbb64d3ca
Jobless <faizumuhammad01@gmail.com>
2026-09-25 22:15:56 +0100

    Update WagmiProviders.tsx

--- components/WagmiProviders.tsx
+++ components/WagmiProviders.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { WagmiProvider, createConfig, http } from "wagmi";
 import { bsc } from "wagmi/chains";
-import { injected } from "wagmi/connectors/injected";
+import { injected } from "wagmi/connectors";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 /**
